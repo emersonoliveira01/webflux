c9befc008982e0f245326d019e8dcd4960e7bd4c
emersonoliveira01 <emersongpi@hotmail.com>
2023-11-27 19:38:36 -0300

    change mapper user mapstruct

--- src/main/java/com/example/webfluxcourse/mapper/UserMapper.java
+++ src/main/java/com/example/webfluxcourse/mapper/UserMapper.java
@@ -4,9 +4,13 @@ import com.example.webfluxcourse.entity.User;
 import com.example.webfluxcourse.model.request.UserRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(
-        componentModel = "spring"
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
 )
 public interface UserMapper {
 
