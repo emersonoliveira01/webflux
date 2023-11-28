a4ca8d6e99c70ff2c2acf34591e4de7b2c0d5776
emersonoliveira01 <emersongpi@hotmail.com>
2023-11-28 11:09:07 -0300

    add impl findAll user contract

--- src/main/java/com/example/webfluxcourse/service/UserService.java
+++ src/main/java/com/example/webfluxcourse/service/UserService.java
@@ -7,6 +7,7 @@ import com.example.webfluxcourse.repository.UserRepository;
 import com.example.webfluxcourse.service.exception.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static java.lang.String.format;
@@ -24,7 +25,11 @@ public class UserService {
 
     public Mono<User> findById(final String id) {
         return userRepository.findById(id)
-                .switchIfEmpty(Mono.error(new ObjectNotFoundException(format("Object not found. Id: %s, Type: %s ",
+                .switchIfEmpty(Mono.error(new ObjectNotFoundException(format("Object not found. Id: %s, Type: %s",
                         id, User.class.getSimpleName()))));
+    }
+
+    public Flux<User> findAll() {
+        return userRepository.findAll();
     }
 }
